f8e8fce36b65e4877db2cb754aeb6e3099e99cfe
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2015-06-18 12:51:23 +0100

    file creation

---
+++ ImageLoader.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class Image:
+    # class used to store the phase and fluorescence images and the different masks needed
+
+    def __init__(self, params):
+
+        self.phase_image = None
+        self.phase_mask = None
+        self.fluorescence_image = None
+
+    def compute_phase_mask(self):
+        pass__author__ = 'Bruno'
