25c98927fcdd6b2ee825e23919c8be3f1b8fa857
Bruno Saraiva <bruno.msaraiva2@gmail.com>
2015-06-18 13:29:23 +0100

    initial modules

--- ImageLoader.py
+++ ImageLoader.py
@@ -6,8 +6,21 @@ class Image:
     def __init__(self, params):
 
         self.phase_image = None
+        self.base_mask = None
         self.phase_mask = None
         self.fluorescence_image = None
+
+    def load_phase(self):
+        pass
+
+    def load_fluorescence(self):
+        pass
+
+    def save_image(self):
+        pass
+
+    def compute_base_mask(self):
+        pass
 
     def compute_phase_mask(self):
         pass
--- ImageProcessing.py
+++ ImageProcessing.py
@@ -3,6 +3,18 @@
 class Cell:
 
     def __init__(self):
+
+        self.cell_mask = None
+        self.perimeter_mask = None
+        self.septum_mask = None
+
+    def compute_cell_mask(self):
+        pass
+
+    def compute_perimeter_mask(self):
+        pass
+
+    def compute_septum_mask(self):
         pass
 
 class ImageCells:
